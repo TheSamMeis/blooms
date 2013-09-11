67ed6e3c9529db32350e03b1b6ded3504008ebf1
Sam Meis <samanthakmeis@gmail.com>
2013-09-11 16:14:13 -0700

    fixed migration file name

--- db/migrate/20130816183808_change_name_schema.rb
+++ db/migrate/20130816183808_change_name_schema.rb
@@ -1,6 +1,7 @@
 class ChangeNameSchema < ActiveRecord::Migration
   def change
 
-  	
+  	rename_column :users, :L_name, :last_name
+  	rename_column :users, :F_name, :first_name
   end
 end
--- db/migrate/20130816215417_change_address_table.rb
+++ db/migrate/20130816215417_change_address_table.rb
@@ -1,10 +1,10 @@
 class ChangeAddressTable < ActiveRecord::Migration
    def change
 
-  	rename_column :ship_adresses, :l_name, :last_name
-  	rename_column :ship_adresses, :f_name, :first_name 
-  	rename_column :ship_adresses, :adress2, :address2 
-   	rename_table :ship_adresses, :ship_addresses
+  	rename_column :ship_addresses, :l_name, :last_name
+  	rename_column :ship_addresses, :f_name, :first_name 
+  	rename_column :ship_addresses, :adress2, :address2 
+   	rename_table :ship_addresses, :ship_addresses
 
     
   end
