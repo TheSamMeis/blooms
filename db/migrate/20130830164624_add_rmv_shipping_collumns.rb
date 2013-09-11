f382a35d421082159a2d8e3678c5bed01ac74389
Sam Meis <samanthakmeis@gmail.com>
2013-09-11 16:24:18 -0700

    fixed migration file name

--- db/migrate/20130830164624_add_rmv_shipping_collumns.rb
+++ db/migrate/20130830164624_add_rmv_shipping_collumns.rb
@@ -1,7 +1,6 @@
 class AddRmvShippingCollumns < ActiveRecord::Migration
   def change
   	add_column :subscriptions, :ship_address_id, :string
-  	remove_column :subscriptions, :shipping_addressb_id
   	remove_column :subscriptions, :shipping_address_id
   	remove_column :subscriptions, :ship_id
   end
