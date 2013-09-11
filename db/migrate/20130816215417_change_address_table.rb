ffc188feb49a330e31a3d87a8c9d8eb866a94503
Sam Meis <samanthakmeis@gmail.com>
2013-09-11 16:18:27 -0700

    fixed migration file name

--- db/migrate/20130816215417_change_address_table.rb
+++ db/migrate/20130816215417_change_address_table.rb
@@ -3,7 +3,6 @@ class ChangeAddressTable < ActiveRecord::Migration
 
   	rename_column :ship_addresses, :l_name, :last_name
   	rename_column :ship_addresses, :f_name, :first_name 
-  	rename_column :ship_addresses, :address2, :address2 
    	rename_table :ship_addresses, :ship_addresses
 
     
